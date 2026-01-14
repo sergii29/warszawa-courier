2b491cde3ebbde72708a80057082abc65ed6dbfc
sergii29 <shalfeyy@gmail.com>
2026-01-14 01:17:04 +0100

    database.js

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
 // --- database.js ---
+// ТОЛЬКО ИНИЦИАЛИЗАЦИЯ FIREBASE
+// Вся логика сохранения теперь в logic.js
 
 const firebaseConfig = {
   apiKey: "AIzaSyChrQMyO2soPgoEHq_f3aYs5Cs19q3sWEk",
@@ -11,47 +13,7 @@ const firebaseConfig = {
   measurementId: "G-TGSCHBVLX2"
 };
 
-// Инициализация
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-
-// Функция сохранения в облако (ТЕПЕРЬ С ИМЕНЕМ!)
-function saveToCloud() {
-    const tg = window.Telegram.WebApp.initDataUnsafe;
-    
-    // Получаем ID
-    let userId = (tg && tg.user) ? tg.user.id : "test_user_from_browser";
-    
-    // Получаем Имя и Никнейм
-    let firstName = (tg && tg.user) ? tg.user.first_name : "Browser Player";
-    let userName = (tg && tg.user && tg.user.username) ? "@" + tg.user.username : "No Username";
-
-    // Собираем всё вместе: Данные игры + Имя
-    let dataToSave = {
-        ...G,           // Деньги, уровень и т.д.
-        name: firstName, // Имя (Sergii)
-        user: userName   // Ник (@sergii)
-    };
-
-    // Отправляем в базу
-    db.ref('users/' + userId).set(dataToSave);
-    console.log("Сохранено: " + firstName);
-}
-
-// Слушаем изменения
-function listenToCloud() {
-    let userId = (window.Telegram.WebApp.initDataUnsafe && window.Telegram.WebApp.initDataUnsafe.user) 
-                 ? window.Telegram.WebApp.initDataUnsafe.user.id 
-                 : "test_user_from_browser";
-
-    db.ref('users/' + userId).on('value', (snapshot) => {
-        const cloudData = snapshot.val();
-        if (cloudData) {
-            if (cloudData.money !== G.money || cloudData.lvl !== G.lvl) {
-                G.money = cloudData.money;
-                G.lvl = cloudData.lvl;
-                updateUI();
-            }
-        }
-    });
-}
+window.db = db; // Делаем доступным глобально
+console.log("Firebase initialized");
