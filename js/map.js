81bb7c5e411438e7bd5275a62b752d9fd1d2f5d3
sergii29 <shalfeyy@gmail.com>
2026-01-22 02:47:55 +0100

    map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,12 @@
+// js/map.js
+export function initMap() {
+  const map = L.map("map", {
+    zoomControl: false
+  }).setView([52.2297, 21.0122], 13);
 
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap"
+  }).addTo(map);
+
+  L.marker([52.2297, 21.0122]).addTo(map);
+}
