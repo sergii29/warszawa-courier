7c0bfc2320b2ffbd5c08633cb61a6282d0b1a271
sergii29 <shalfeyy@gmail.com>
2026-01-22 02:49:14 +0100

    ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,18 @@
+// js/ui.js
+export function initUI() {
+  const menuBtn = document.getElementById("menuBtn");
+  const sideMenu = document.getElementById("sideMenu");
+  const overlay = document.getElementById("overlay");
 
+  if (!menuBtn) return;
+
+  menuBtn.onclick = () => {
+    sideMenu.classList.add("open");
+    overlay.classList.add("show");
+  };
+
+  overlay.onclick = () => {
+    sideMenu.classList.remove("open");
+    overlay.classList.remove("show");
+  };
+}
