fe42314e615b216712009e3d21f176e71faf11e7
sergii29 <shalfeyy@gmail.com>
2026-02-01 01:49:14 +0100

    save.js

---
+++ src/save.js
@@ -0,0 +1,15 @@
+import { db } from "./firebase.js";
+import { ref, set, get } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const USER_ID = "test_user"; // позже заменим на Telegram ID
+
+export function saveState(state) {
+  return set(ref(db, "users/" + USER_ID), state);
+}
+
+export async function loadState(state) {
+  const snapshot = await get(ref(db, "users/" + USER_ID));
+  if (snapshot.exists()) {
+    Object.assign(state, snapshot.val());
+  }
+}
