24e555ab36d52cbca2f872cbce861c17e1a0fc30
sergii29 <shalfeyy@gmail.com>
2026-02-01 01:21:09 +0100

    firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChrQMyO2soPgoEHq_f3aYs5Cs19q3sWEk",
+  authDomain: "warszawa-courier.firebaseapp.com",
+  databaseURL: "https://warszawa-courier-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "warszawa-courier",
+  storageBucket: "warszawa-courier.firebasestorage.app",
+  messagingSenderId: "295860613508",
+  appId: "1:295860613508:web:86fe8364377d6875612dd1",
+  measurementId: "G-TGSCHBVLX2"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+
+console.log("Firebase OK");
