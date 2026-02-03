2332df0ae3e5dae7aed779b11c6ec31a574bf94c
sergii29 <shalfeyy@gmail.com>
2026-02-03 23:02:55 +0100

    manifest.js

--- patches/manifest.js
+++ patches/manifest.js
@@ -1,11 +1,11 @@
 const patches = [
-    'ultra.js'
+    'game_v1.js'
 ];
 
 patches.forEach(file => {
     const script = document.createElement('script');
-    // Добавляем случайное число, чтобы кэш обновился 100%
-    script.src = 'patches/' + file + '?time=' + Date.now();
+    // Добавляем время, чтобы сбить кэш наверняка
+    script.src = 'patches/' + file + '?v=' + Date.now();
     script.async = false;
     document.body.appendChild(script);
 });
