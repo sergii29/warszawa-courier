1ab7c530c06748152b070e41a0226c6454297cd8
sergii29 <shalfeyy@gmail.com>
2026-02-03 22:44:42 +0100

    manifest.js

--- patches/manifest.js
+++ patches/manifest.js
@@ -1,10 +1,13 @@
+// MANIFEST: FORCE RELOAD
 const patches = [
     'release.js'
 ];
 
 patches.forEach(file => {
     const script = document.createElement('script');
-    script.src = 'patches/' + file;
+    // Добавляем время к ссылке, чтобы браузер не брал старое из памяти
+    script.src = 'patches/' + file + '?v=' + Date.now();
     script.async = false;
     document.body.appendChild(script);
+    console.log(`[Patch System] Loading NEW version: ${file}`);
 });
