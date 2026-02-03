cda344b1f552a6c83dd1f9db6f6ca1139999ab48
sergii29 <shalfeyy@gmail.com>
2026-02-03 22:23:14 +0100

    manifest.js

--- patches/manifest.js
+++ patches/manifest.js
@@ -1,15 +1,10 @@
-// Папка для патчей.
-// Мы грузим ТОЛЬКО ВТОРОЙ файл, потому что я собрал в него всё сразу (и банк, и магазин).
-
 const patches = [
-    'game-update-v2.js' 
+    'fix_v3.js'
 ];
 
-// Логика загрузчика
 patches.forEach(file => {
     const script = document.createElement('script');
     script.src = 'patches/' + file;
     script.async = false;
     document.body.appendChild(script);
-    console.log(`[Patch System] Loading: ${file}`);
 });
